0f75052be30cfeb1f89db272240da6b430dd13af
MaiXuanNghia <nghiamaiquangtri@gmail.com>
2024-01-30 08:01:48 +0700

    bài 1 cài đặt laravel

---
+++ learning_laravel/resources/views/home.blade.php
@@ -0,0 +1,1 @@
+<h1 style="text-align: center; font-size:50px; color:blue"> Học lập trình với laravel</h1>
---
+++ learning_laravel/resources/views/product.blade.php
@@ -0,0 +1,7 @@
+<div style="display:flex;">
+    <?php for ($i = 0; $i < 10; $i++) :  ?>
+
+        <p style="color: red; padding:2%">product</p>
+
+    <?php endfor ?>
+</div>
