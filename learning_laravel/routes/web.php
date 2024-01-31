6af4461912f34072c476488183e850aa78dfe3f2
MaiXuanNghia <nghiamaiquangtri@gmail.com>
2024-01-31 17:44:28 +0700

    học Laravel bài 7

--- learning_laravel/routes/web.php
+++ learning_laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProductsController;
+use App\Http\Controllers\Admin\DasboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return "<h1>Trang chủ lỗi</h1>";
+})->name('home');
+
+Route::middleware('auth.admin')->prefix('/admin')->group(function () {
+    Route::get('/', [DasboardController::class, 'index']);
+    Route::resource('/products', ProductsController::class)->middleware('auth.admin.products');
 });
